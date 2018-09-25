0873b7afd9c09b65ab19e576675ab1cdb6f4a3d2
Andy Tuttle <andytuttle12@gmail.com>
2018-09-24 20:55:24 -0400

    Simplified context wrapper.

--- src/components/CardDashboard/index.jsx
+++ src/components/CardDashboard/index.jsx
@@ -6,38 +6,14 @@ const wrappedContext = React.createContext();
 
 export default class CardDashboard extends Component {
   static propTypes = {
-    title: PropTypes.string,
     children: PropTypes.instanceOf(Object).isRequired,
   };
 
   render() {
-    const {
-      props: {
-        title,
-        children,
-      },
-    } = this;
-
     return (
       <wrappedContext.Provider>
-        <div>
-          className={`card-dashboard-root`}
-        >
-          {title && (
-            <React.Fragment>
-              <div className="card-dashboard-header">
-                <div className="card-dashboard-title">{title}</div>
-              </div>
-              <div className="card-dashboard-body">
-                {children}
-              </div>
-            </React.Fragment>
-          )}
-          {!title && (
-            <div className="card-dashboard-body alone">
-              {children}
-            </div>
-          )}
+        <div className="context-root">
+          {this.props.children}
         </div>
       </wrappedContext.Provider>
     );
